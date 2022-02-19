820dd972004d84673eb0e931d40f58299955beb8
yusuke <k82.y12@gmail.com>
2022-02-18 17:50:49 -0800

    initial commit

---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+import classes.Question;
+
+public class Driver {
+    public static void main(String[] args) {
+        Question test = new Question();
+        System.out.println(test.getAnswer());
+    }
+}
