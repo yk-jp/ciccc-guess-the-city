c90965575831af797c5ea2dc3e56e8a3a3a0b29a
yusuke <k82.y12@gmail.com>
2022-02-18 17:50:49 -0800

    initial commit

---
+++ src/classes/Question.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Question {
+    private String answer;
+
+    public Question() {
+        this.answer = "hello world";
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
