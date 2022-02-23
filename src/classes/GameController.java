4b5117c2f563b101c05f9055690ff1b158403507
yusuke <k82.y12@gmail.com>
2022-02-23 15:11:54 -0800

    read file and display city name with udnerscore

--- src/classes/GameController.java
+++ src/classes/GameController.java
@@ -1,4 +1,34 @@
 package classes;
 
 public class GameController {
+
+    public Question question;
+
+    public GameController(Question question) {
+        this.question = question;
+    }
+
+    public void start() {
+        this.initialSetup();
+
+        System.out.println(this.question.getCurrResult());
+        System.out.println(this.question.getAnswer());
+    }
+
+    public void initialSetup() {
+        this.question.resetAnswer();
+        this.convertLettersToUnderScore();
+    }
+
+
+    private void convertLettersToUnderScore() {
+        String answer = this.question.getAnswer();
+        String underScores = "";
+
+        for (int i = 0; i < answer.length(); i++) {
+            underScores += "_";
+        }
+
+        this.question.setCurrResult(underScores);
+    }
 }
